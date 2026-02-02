d90dd4ff570c614e65c8d482a421484d2b5bb65a
AnhQuoc2903 <133018919+AnhQuoc2903@users.noreply.github.com>
2026-02-02 08:44:19 +0700

    Add AR Polycam section

--- src/data/tours.js
+++ src/data/tours.js
@@ -17,4 +17,16 @@ export const tours = [
     icon: "🖼️",
     url: "https://trienlam3d.metaart.vn/",
   },
+  {
+    id: "ar-polycam-1",
+    title: "AR Mô hình 1",
+    icon: "📱",
+    url: "https://poly.cam/capture/aa7b72ba-cd18-400d-9552-d805bdc09d7d",
+  },
+  {
+    id: "ar-polycam-2",
+    title: "AR Mô hình 2",
+    icon: "📱",
+    url: "https://poly.cam/capture/782aa845-03fd-42b2-b152-cb39aecb7835",
+  },
 ];
