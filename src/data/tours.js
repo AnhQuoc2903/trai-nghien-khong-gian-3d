9086f265ee0f2f3c6767f7d48c642080a7015e86
AnhQuoc2903 <133018919+AnhQuoc2903@users.noreply.github.com>
2026-02-02 13:28:10 +0700

    comment noi that nha 3d

--- src/data/tours.js
+++ src/data/tours.js
@@ -5,12 +5,12 @@ export const tours = [
     icon: "🏫",
     url: "https://nhathieunhi.metaart.vn/",
   },
-  {
-    id: "noi-that-nha-3d",
-    title: "Nội Thất Nhà 3D",
-    icon: "🏠",
-    url: "https://noithatnha3d.metaart.vn/",
-  },
+  // {
+  //   id: "noi-that-nha-3d",
+  //   title: "Nội Thất Nhà 3D",
+  //   icon: "🏠",
+  //   url: "https://noithatnha3d.metaart.vn/",
+  // },
   {
     id: "trien-lam-3d",
     title: "Triển Lãm 3D",
